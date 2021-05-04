9e9559121969ce4277baa9c0f088d19be81694a3
dungvhv <dungvhv@gmail.com>
2021-05-04 20:53:32 +0700

    up bai test online

---
+++ TESTONLINE/index.js
@@ -0,0 +1,22 @@
+// 1A Chưa làm được
+
+// 2A
+// function merge(a, b) {
+//     var i,
+//         l = Math.min(a.length, b.length),
+//         temp = '';
+
+//     for( i = 0; i < l; i++) {
+//         temp += a[i] + b[i];
+//     }
+//     return temp + a.slice(i) + b.slice(i);
+// }
+
+// console.log(merge('abc', '123')); 
+// console.log(merge('abc', '0123'));  
+// console.log(merge('abcd', '123')); 
+
+// B
+function quaySo() {
+
+}
